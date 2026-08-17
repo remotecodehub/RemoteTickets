c0da9c68093ba85d1cc546b62f0285442f65c963
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-08-16 21:23:17 -0300

    fix: include filesystem APIs required by setup persistence

--- src/server/RemoteTickets.Infrastructure/GlobalUsings.cs
+++ src/server/RemoteTickets.Infrastructure/GlobalUsings.cs
@@ -2,6 +2,7 @@ global using System;
 global using System.Collections.Concurrent;
 global using System.Collections.Generic;
 global using System.IdentityModel.Tokens.Jwt;
+global using System.IO;
 global using System.Linq;
 global using System.Linq.Expressions;
 global using System.Security.Claims;
